4057766168674336bde41705c7ef0d6a1b52fe42
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2020-12-24 06:22:28 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,113 +1,68 @@
-import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-//import slugify from 'slugify';
-
-import './App.css';
-import Header from './Header';
-import MainForm from './MainForm';
-import MainSummary from './MainSummary';
+import React, { Component } from "react";
+import MainForm from "./Components/MainForm";
+import FEATURES from "./Components/FEATURES";
+import "./App.css";
+import MainSummary from "./Components/MainSummary";
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
 
 class App extends Component {
   state = {
     selected: {
       Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
+        name: "17th Generation Intel Core HB (7 Core with donut spare)",
+        cost: 700,
       },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
+      "Operating System": {
+        name: "Ubuntu Linux 16.04",
+        cost: 200,
       },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
+      "Video Card": {
+        name: "Toyota Corolla 1.5v",
+        cost: 1150.98,
       },
       Display: {
         name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
-      }
-    }
+        cost: 1500,
+      },
+    },
   };
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
-      selected
+      selected,
     });
   };
 
   render() {
-
-    /**** Moved to MainForm 
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });*/
-
-      /****   Moved to Feature 
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });*/
-
-    /****  Moved to MainSummary 
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });*/
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
-
     return (
       <div className="App">
-        <Header />
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
         <main>
-          <MainForm />
-          <MainSummary />
+          <MainForm
+            className="main_form"
+            USCurrencyFormat={USCurrencyFormat}
+            updateFeature={this.updateFeature}
+            features={FEATURES}
+            {...this.props}
+            {...this.state}
+          />
+          <MainSummary
+            className="main_summary"
+            USCurrencyFormat={USCurrencyFormat}
+            features={FEATURES}
+            {...this.props}
+            {...this.state}
+          />
         </main>
       </div>
     );
